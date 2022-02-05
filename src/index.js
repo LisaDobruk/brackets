0194d715d85b80f20a3bb2f77b9d01bd85bbd173
LisaDobruk <liskam8525gmail.com>
2022-02-06 01:30:32 +0300

    cool

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+let bracketsPair = {};
+for(let i = 0; i < bracketsConfig.length; i++){
+    let key = [bracketsConfig[i][0]];
+    let value = bracketsConfig[i][1];
+   bracketsPair[key] = value;
+}
+
+let stack = [];
+  for(let i = 0; i < str.length; i++){
+      let currentElement = str[i];
+      let topElement = stack[stack.length - 1];
+      let partBrecket = bracketsPair[topElement];
+     if(partBrecket === currentElement){
+              stack.pop();
+      }else{
+          stack.push(currentElement);
+      }
+  }
+  return stack.length === 0;
 }
